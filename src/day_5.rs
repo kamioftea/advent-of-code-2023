2fd7c8f40ae5b11bbf70fdc8473201e0cea41383
Jeff Horton <jeff@goblinoid.co.uk>
2023-12-05 21:19:53 +0000

    Doc comments

--- src/day_5.rs
+++ src/day_5.rs
@@ -1,6 +1,15 @@
-//! This is my solution for [Advent of Code - Day 5: _???_](https://adventofcode.com/2023/day/5)
+//! This is my solution for [Advent of Code - Day 5: _If You Give A Seed A Fertilizer_](https://adventofcode.com/2023/day/5)
 //!
+//! Given input representing a list of [`Seed`] ids and an [`Almanac`] of mappings between ids in pairs of
+//! [`Category`]s. Transform the [`Seed`] ids to [`Location`] ids and find the lowest [`Location`] id to solve the
+//! puzzle.
 //!
+//! The parsing starts with [`parse_input`], which delegates to [`parse_seeds`], [`parse_almanac`], [`parse_header`],
+//! and [`parse_range`].
+//!
+//! The seed ids are interpreted for part one using [`ids_as_single_seeds`], and part two with [`ids_to_ranges`]. These
+//! seed ranges are turned into the minimum location by [`find_nearest_location`], using
+//! [`progress_id_ranges_to_category`] and [`progress_id_range`].
 
 use crate::day_5::Category::*;
 use itertools::Itertools;
@@ -9,23 +18,25 @@ use std::fmt::Debug;
 use std::fs;
 use std::str::FromStr;
 
+/// A range of ids to modify when applying the mapping for an AlmanacSection
 #[derive(Eq, PartialEq, Debug)]
 struct AlmanacRange {
     start: i64,
-    delta: i64,
     length: i64,
+    delta: i64,
 }
 
 impl AlmanacRange {
-    fn new(start: i64, delta: i64, length: i64) -> AlmanacRange {
+    fn new(start: i64, length: i64, delta: i64) -> AlmanacRange {
         AlmanacRange {
             start,
-            delta,
             length,
+            delta,
         }
     }
 }
 
+/// A mapping from one category of ids to another. Ranges are stored sorted by starting source id
 #[derive(Eq, PartialEq, Debug)]
 struct AlmanacSection {
     source: Category,
@@ -43,8 +54,10 @@ impl AlmanacSection {
     }
 }
 
+/// A collection of category mappings grouped by source category
 type Almanac = HashMap<Category, AlmanacSection>;
 
+/// The possible categories of id
 #[derive(Eq, PartialEq, Debug, Hash, Copy, Clone)]
 enum Category {
     Seed,
@@ -75,6 +88,7 @@ impl FromStr for Category {
     }
 }
 
+/// A range of ids in a category that should be planted
 #[derive(Eq, PartialEq, Debug, Clone)]
 struct IdRange {
     category: Category,
@@ -103,29 +117,24 @@ pub fn run() {
 
     println!(
         "The nearest location id from individual seeds is: {}",
-        find_nearest_location_from_ranges(ids_as_single_seeds(&seeds), &almanac)
+        find_nearest_location(ids_as_single_seeds(&seeds), &almanac)
     );
 
     println!(
         "The nearest location id from seed ranges is: {}",
-        find_nearest_location_from_ranges(ids_to_ranges(&seeds), &almanac)
+        find_nearest_location(ids_to_ranges(&seeds), &almanac)
     )
 }
 
-fn find_nearest_location_from_ranges(seeds: Vec<IdRange>, almanac: &Almanac) -> i64 {
-    progress_id_ranges_to(seeds, Location, almanac)
-        .iter()
-        .map(|range| range.start)
-        .min()
-        .unwrap()
-}
-
+/// Split and parse the puzzle input into the list of seeds and the almanac specification.
+/// The seeds and each section are delimited by blank lines.
 fn parse_input(input: &String) -> (Vec<i64>, Almanac) {
     let mut parts = input.split("\n\n");
 
-    (parse_seeds(parts.next().unwrap()), parse_maps(parts))
+    (parse_seeds(parts.next().unwrap()), parse_almanac(parts))
 }
 
+/// Parse the list of seeds to numeric ids
 fn parse_seeds(input: &str) -> Vec<i64> {
     input
         .split(" ")
@@ -133,13 +142,14 @@ fn parse_seeds(input: &str) -> Vec<i64> {
         .collect()
 }
 
-fn parse_maps<'a>(map_specs: impl Iterator<Item = &'a str>) -> Almanac {
-    let mut maps = HashMap::new();
+/// Each almanac section is a single header line, then one line per id mapping
+fn parse_almanac<'a>(section_specs: impl Iterator<Item = &'a str>) -> Almanac {
+    let mut almanac = HashMap::new();
 
-    for map_spec in map_specs {
-        let mut lines = map_spec.lines();
+    for section_spec in section_specs {
+        let mut lines = section_spec.lines();
         let (from, to) = parse_header(lines.next().unwrap());
-        maps.insert(
+        almanac.insert(
             from.clone(),
             AlmanacSection::new(
                 from,
@@ -152,24 +162,75 @@ fn parse_maps<'a>(map_specs: impl Iterator<Item = &'a str>) -> Almanac {
         );
     }
 
-    maps
+    almanac
 }
 
+/// Parse a header in the format `seed-to-soil map:` into source and destination categories
 fn parse_header(header_spec: &str) -> (Category, Category) {
     let mapping_part = header_spec.replace(" map:", "");
     let (from, to) = mapping_part.split_once("-to-").unwrap();
     return (from.parse().unwrap(), to.parse().unwrap());
 }
 
+/// Parse a range of id mappings, three space-separated numbers in the order `destination_start` `source_start` `length`
 fn parse_range(range_spec: &str) -> AlmanacRange {
     let parts: Vec<i64> = range_spec
         .split(" ")
         .filter_map(|part| part.parse().ok())
         .collect();
 
-    AlmanacRange::new(parts[1], parts[0] - parts[1], parts[2])
+    AlmanacRange::new(parts[1], parts[2], parts[0] - parts[1])
 }
 
+/// For part one each seed is a single id, which can be represented as a range of length 1
+fn ids_as_single_seeds(ids: &Vec<i64>) -> Vec<IdRange> {
+    ids.into_iter()
+        .map(|&start| IdRange::new(Seed, start, 1))
+        .collect()
+}
+
+/// For part two each pair of numbers represents a range, in the format `start length`
+fn ids_to_ranges(ids: &Vec<i64>) -> Vec<IdRange> {
+    ids.into_iter()
+        .tuples()
+        .map(|(&start, &length)| IdRange::new(Seed, start, length))
+        .collect()
+}
+
+/// Apply all almanac mappings, return the start of the lowest resulting range
+fn find_nearest_location(seeds: Vec<IdRange>, almanac: &Almanac) -> i64 {
+    progress_id_ranges_to_category(seeds, Location, almanac)
+        .iter()
+        .map(|range| range.start)
+        .min()
+        .unwrap()
+}
+
+/// Recursively advance a list of category ids until a specific category is reached
+fn progress_id_ranges_to_category(
+    id_ranges: Vec<IdRange>,
+    category: Category,
+    almanac: &Almanac,
+) -> Vec<IdRange> {
+    let current_category = id_ranges.get(0).unwrap().category;
+
+    if current_category == category {
+        id_ranges
+    } else {
+        progress_id_ranges_to_category(
+            id_ranges
+                .iter()
+                .flat_map(|range| progress_id_range(range, almanac))
+                .collect(),
+            category,
+            almanac,
+        )
+    }
+}
+
+/// Take a single range of ids to plant in one category and apply the relevant mapping from the almanac.
+/// - Where different mappings apply to different parts of the range, return a separate continuous range for each
+/// - Where a mapping is not defined, the id doesn't change, but the category still advances
 fn progress_id_range(id_range: &IdRange, almanac: &Almanac) -> Vec<IdRange> {
     let mut new_id_ranges = Vec::new();
     let mut current = id_range.start;
@@ -177,7 +238,9 @@ fn progress_id_range(id_range: &IdRange, almanac: &Almanac) -> Vec<IdRange> {
 
     let section = almanac.get(&id_range.category).unwrap();
 
+    // ranges are already sorted, so walk through them adding mapped ranges where they overlap
     for almanac_range in &section.ranges {
+        // if there is a gap before the next id starts add an output range with unchanged ids
         if almanac_range.start > current {
             let id_sub_range_end = almanac_range.start.min(id_range_end);
             new_id_ranges.push(IdRange::new(
@@ -189,10 +252,12 @@ fn progress_id_range(id_range: &IdRange, almanac: &Almanac) -> Vec<IdRange> {
             current = almanac_range.start;
         }
 
+        // Short circuit if we've reached the end of the input range
         if current >= id_range_end {
             break;
         }
 
+        // if there is an overlap, add the overlap to the output
         if almanac_range.start + almanac_range.length > current {
             let id_range_end = (almanac_range.start + almanac_range.length).min(id_range_end);
             new_id_ranges.push(IdRange::new(
@@ -204,11 +269,13 @@ fn progress_id_range(id_range: &IdRange, almanac: &Almanac) -> Vec<IdRange> {
             current = almanac_range.start + almanac_range.length;
         }
 
+        // Short circuit if we've reached the end of the input range
         if current >= id_range_end {
             break;
         }
     }
 
+    // If the current range extends beyond the mappings in the almanac, add the remaining ids unmapped to the output
     if current < id_range_end {
         new_id_ranges.push(IdRange::new(
             section.destination,
@@ -218,40 +285,6 @@ fn progress_id_range(id_range: &IdRange, almanac: &Almanac) -> Vec<IdRange> {
     }
 
     new_id_ranges
-}
-
-fn ids_as_single_seeds(ids: &Vec<i64>) -> Vec<IdRange> {
-    ids.into_iter()
-        .map(|&start| IdRange::new(Seed, start, 1))
-        .collect()
-}
-
-fn ids_to_ranges(ids: &Vec<i64>) -> Vec<IdRange> {
-    ids.into_iter()
-        .tuples()
-        .map(|(&start, &length)| IdRange::new(Seed, start, length))
-        .collect()
-}
-
-fn progress_id_ranges_to(
-    id_ranges: Vec<IdRange>,
-    category: Category,
-    almanac: &Almanac,
-) -> Vec<IdRange> {
-    let current_category = id_ranges.get(0).unwrap().category;
-
-    if current_category == category {
-        id_ranges
-    } else {
-        progress_id_ranges_to(
-            id_ranges
-                .iter()
-                .flat_map(|range| progress_id_range(range, almanac))
-                .collect(),
-            category,
-            almanac,
-        )
-    }
 }
 
 #[cfg(test)]
@@ -270,7 +303,7 @@ mod tests {
                 AlmanacSection::new(
                     Seed,
                     Soil,
-                    vec![AlmanacRange::new(50, 2, 48), AlmanacRange::new(98, -48, 2)],
+                    vec![AlmanacRange::new(50, 48, 2), AlmanacRange::new(98, 2, -48)],
                 ),
             ),
             (
@@ -279,9 +312,9 @@ mod tests {
                     Soil,
                     Fertilizer,
                     vec![
-                        AlmanacRange::new(0, 39, 15),
-                        AlmanacRange::new(15, -15, 37),
-                        AlmanacRange::new(52, -15, 2),
+                        AlmanacRange::new(0, 15, 39),
+                        AlmanacRange::new(15, 37, -15),
+                        AlmanacRange::new(52, 2, -15),
                     ],
                 ),
             ),
@@ -291,10 +324,10 @@ mod tests {
                     Fertilizer,
                     Water,
                     vec![
-                        AlmanacRange::new(0, 42, 7),
-                        AlmanacRange::new(7, 50, 4),
-                        AlmanacRange::new(11, -11, 42),
-                        AlmanacRange::new(53, -4, 8),
+                        AlmanacRange::new(0, 7, 42),
+                        AlmanacRange::new(7, 4, 50),
+                        AlmanacRange::new(11, 42, -11),
+                        AlmanacRange::new(53, 8, -4),
                     ],
                 ),
             ),
@@ -303,7 +336,7 @@ mod tests {
                 AlmanacSection::new(
                     Water,
                     Light,
-                    vec![AlmanacRange::new(18, 70, 7), AlmanacRange::new(25, -7, 70)],
+                    vec![AlmanacRange::new(18, 7, 70), AlmanacRange::new(25, 70, -7)],
                 ),
             ),
             (
@@ -312,9 +345,9 @@ mod tests {
                     Light,
                     Temperature,
                     vec![
-                        AlmanacRange::new(45, 36, 19),
-                        AlmanacRange::new(64, 4, 13),
-                        AlmanacRange::new(77, -32, 23),
+                        AlmanacRange::new(45, 19, 36),
+                        AlmanacRange::new(64, 13, 4),
+                        AlmanacRange::new(77, 23, -32),
                     ],
                 ),
             ),
@@ -323,7 +356,7 @@ mod tests {
                 AlmanacSection::new(
                     Temperature,
                     Humidity,
-                    vec![AlmanacRange::new(0, 1, 69), AlmanacRange::new(69, -69, 1)],
+                    vec![AlmanacRange::new(0, 69, 1), AlmanacRange::new(69, 1, -69)],
                 ),
             ),
             (
@@ -331,7 +364,7 @@ mod tests {
                 AlmanacSection::new(
                     Humidity,
                     Location,
-                    vec![AlmanacRange::new(56, 4, 37), AlmanacRange::new(93, -37, 4)],
+                    vec![AlmanacRange::new(56, 37, 4), AlmanacRange::new(93, 4, -37)],
                 ),
             ),
         ]
@@ -411,15 +444,12 @@ humidity-to-location map:
     #[test]
     fn can_find_nearest_location_from_ranges() {
         assert_eq!(
-            find_nearest_location_from_ranges(
-                ids_as_single_seeds(&example_seeds()),
-                &example_almanac()
-            ),
+            find_nearest_location(ids_as_single_seeds(&example_seeds()), &example_almanac()),
             35
         );
 
         assert_eq!(
-            find_nearest_location_from_ranges(ids_to_ranges(&example_seeds()), &example_almanac()),
+            find_nearest_location(ids_to_ranges(&example_seeds()), &example_almanac()),
             46
         );
     }
